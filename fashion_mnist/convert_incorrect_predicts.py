6fd37b9150698f1fcd1eea64bdd2dc84d4281450
mpppk <niboshiporipori@gmail.com>
2021-02-05 12:41:06 +0900

    Add jsonl file and update readme

--- fashion_mnist/convert_incorrect_predicts.py
+++ fashion_mnist/convert_incorrect_predicts.py
@@ -3,13 +3,25 @@ import sys
 import json
 
 csv_path = sys.argv[1]
+
 df = pd.read_csv(csv_path)
 
-for index, row in df.iterrows():
-    d = {
-        "path": row["path"],
-        "boundingBoxes": [
-            {"tagName": "inference-failed:" + row["predict"]}
-        ]
-    }
-    print(json.dumps(d))
+
+def to_json_rows(df):
+    for index, row in df.iterrows():
+        yield {
+            "path": row["path"],
+            "boundingBoxes": [
+                {"tagName": "inference-failed:" + row["predict"]}
+            ]
+        }
+
+
+if len(sys.argv) > 2:
+    output_path = sys.argv[2]
+    with open(output_path, mode='w') as f:
+        for d in to_json_rows(df):
+            f.write(json.dumps(d)+'\n')
+else:
+    for d in to_json_rows(df):
+        print(json.dumps(d))
